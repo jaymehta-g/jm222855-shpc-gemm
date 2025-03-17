ef360e587c305b5fa2fdd2912733f5b63b9566a3
Jay <91770194+jaymehta-g@users.noreply.github.com>
2025-03-17 12:28:34 -0500

    f

--- src/shpc_gemm.c
+++ src/shpc_gemm.c
@@ -1,4 +1,29 @@
-#include "assignment1.h"
+#include "assignment3.h"
+
+#define MR 4
+#define NR 4
+
+// multiply k by MR on NR by k
+// expects column-major A and B
+static void mkernel(double *A, double* B, double* C, int k) {
+    int csA, rsB = k;
+    int rsA = MR;
+    int csB = NR;
+    for (int i = 0; i < MR; i++)
+    {
+        for (int j = 0; j < NR; j++)
+        {
+            double dot = 0;
+            for (int p = 0; p < k; p++)
+            {
+                mk
+            }
+            
+        }
+        
+    }
+    
+}
 
 void shpc_dgemm( int m, int n, int k,                                            
                     double *A, int rsA, int csA,                                
