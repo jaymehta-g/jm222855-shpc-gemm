91eb3842cd553a8213d741cf0596b535f7e0453e
Jay <91770194+jaymehta-g@users.noreply.github.com>
2025-03-18 11:35:46 -0500

    load store registers

--- src/shpc_gemm.c
+++ src/shpc_gemm.c
@@ -8,26 +8,37 @@
 
 // multiply k by MR on NR by k
 // expects column-major A and row-major B
-void mkernel(double *A, double* B, double* C, int k) {
-    int rsA, rsB = 1;
-    int csA = MR;
-    int csB = k;
-    int rsC = 1;
-    int csC = MR;
-    for (int i = 0; i < MR; i++)
-    {
-        for (int j = 0; j < NR; j++)
-        {
-            double dot = 0;
-            for (int p = 0; p < k; p++)
-            {
-                dot += RC(A, i, p) * RC(B, p, j);
-            }
-            RC(C, i, j) += dot;
-        }
-        
-    }
-    
+// excepts column major C
+// A is MR x k, B is k x NR
+void mkernel(double *A, double* B, double* C, int rsC, int csC, int k) {
+    const int rsA, csB = 1;
+    const csA = MR;
+    const rsB = NR;
+    __m256d c_0_0 = _mm256_loadu_pd(&RC(C, 0, 0));
+    __m256d c_0_1 = _mm256_loadu_pd(&RC(C, 0, 1));
+    __m256d c_0_2 = _mm256_loadu_pd(&RC(C, 0, 2));
+    __m256d c_0_3 = _mm256_loadu_pd(&RC(C, 0, 3));
+    __m256d c_0_4 = _mm256_loadu_pd(&RC(C, 0, 4));
+    __m256d c_0_5 = _mm256_loadu_pd(&RC(C, 0, 5));
+    __m256d c_1_0 = _mm256_loadu_pd(&RC(C, 4, 0));
+    __m256d c_1_1 = _mm256_loadu_pd(&RC(C, 4, 1));
+    __m256d c_1_2 = _mm256_loadu_pd(&RC(C, 4, 2));
+    __m256d c_1_3 = _mm256_loadu_pd(&RC(C, 4, 3));
+    __m256d c_1_4 = _mm256_loadu_pd(&RC(C, 4, 4));
+    __m256d c_1_5 = _mm256_loadu_pd(&RC(C, 4, 5));
+
+    _mm256_storeu_pd(&RC(C,0,0), c_0_0);
+    _mm256_storeu_pd(&RC(C,0,1), c_0_1);
+    _mm256_storeu_pd(&RC(C,0,2), c_0_2);
+    _mm256_storeu_pd(&RC(C,0,3), c_0_3);
+    _mm256_storeu_pd(&RC(C,0,4), c_0_4);
+    _mm256_storeu_pd(&RC(C,0,5), c_0_5);
+    _mm256_storeu_pd(&RC(C,4,0), c_1_0);
+    _mm256_storeu_pd(&RC(C,4,1), c_1_1);
+    _mm256_storeu_pd(&RC(C,4,2), c_1_2);
+    _mm256_storeu_pd(&RC(C,4,3), c_1_3);
+    _mm256_storeu_pd(&RC(C,4,4), c_1_4);
+    _mm256_storeu_pd(&RC(C,4,5), c_1_5);
 }
 
 void shpc_dgemm( int m, int n, int k,                                            
